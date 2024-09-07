6a592ccafeacd71137039aa57f1df0b0fc5b38ac
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-07 08:22:44 +0100

    chore: Update MongoDB connection to use environment variable

--- starter/app.js
+++ starter/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { tasks } from './routes/tasks.js';
 import { connectDB } from './db/connect.js'; // connect to the database
+import 'dotenv/config' // Load environment variables from a .env file into process.env
 
 const app = express();
 const port = 3000; // Port 3000 is the default port for Express.js
@@ -14,7 +15,7 @@ app.use('/api/v1/tasks', tasks);
 // Start the application by connecting to the database and listening on the specified port
 const start = async () => {
   try {
-    await connectDB(); // Attempt to connect to the MongoDB database
+    await connectDB(process.env.MONGO_URI); // Connect to MongoDB using the MONGO_URI environment variable
     console.log('Database connected successfully ...'); // Log successful connection
     app.listen(port, () => { // Start the server
       console.log(`Server is running on port ${port} ...`); // Log server start
--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,7 +1,5 @@
 import { mongoose } from "mongoose";
 
-const connectionString = 'mongodb+srv://Azeez-Samad:%23Samad2004.com@task-manager-api.9mn3l.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=Task-Manager-API'
-
-export const connectDB = ()=>{
-    return mongoose.connect(connectionString)
+export const connectDB = (url)=>{
+    return mongoose.connect(url);
 };
