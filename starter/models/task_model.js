76b2b2275c942955928802272b0753a3cdfeb58f
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-07 18:41:37 +0100

    Refactor all task_controller.js functions to use async/await syntax for better error handling and connect to the database.

--- starter/models/task_model.js
+++ starter/models/task_model.js
@@ -2,8 +2,17 @@ import { mongoose } from "mongoose";
 
 // This is the schema for the task model
 const taskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    // Define the properties of the task model
+    name: {
+        type: String,
+        required: [true, 'A task must have a name'],
+        trim: true,
+        maxlength: [20, 'A task name must have less or equal then 20 characters'],
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 export const task_model = mongoose.model('Task', taskSchema); 
